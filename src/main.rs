1eed88d15171f129ee7d18fd8f943490cddffa34
Kevin Sullivan <kevinsullivan@ksulldev.space>
2023-09-29 20:11:12 +0100

    created function to start the database

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::path::Path;
+use std::fs;
+
 fn main() {
     println!("Hello, world!");
+    create_database();
 }
+
+fn create_database() {
+    // essentially it would create a directory?
+    let path: &Path = Path::new("./database");
+    fs::create_dir_all(path).unwrap();
+    // Ok(())
+}
+
